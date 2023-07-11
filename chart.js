730c77995649d1af5a5991b21545437dcc932f88
dngst <dngst@protonmail.com>
2023-07-12 00:23:33 +0300

    Add dashboard

---
+++ chart.js
@@ -0,0 +1,44 @@
+async function drawDashboard() {
+  const dataset = await d3.csv("game_data.csv")
+
+  const wrapper = d3.select("#wrapper")
+
+  const section = wrapper.append("section");
+
+  function drawMetric(metric) {
+    const { key, title, note } = metric;
+
+    const article = section.append("article");
+    article.append("h3").text(title);
+    article.attr("id", metric.key)
+
+    const value = dataset[0][metric.key]
+    article.append("p").text(value);
+
+    article.append("p").text(note);
+  }
+
+  const metrics = [
+    {
+      key: "percentage",
+      title: "Progress (%)",
+      note: "out of 101%"
+    },
+    {
+      key: "minutes",
+      title: "Time (M)",
+      note: "Minutes"
+    },
+    {
+      key: "seconds",
+      title: "Time (S)",
+      note: "Seconds"
+    },
+  ];
+
+  metrics.forEach((metric) => {
+    drawMetric(metric);
+  });
+}
+
+drawDashboard()
