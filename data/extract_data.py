091e731470769992a0ffb82e2b71e74cb07cfccd
dngst <dngst@protonmail.com>
2023-10-16 19:35:18 +0300

    Extract from 3 save files at once

--- data/extract_data.py
+++ data/extract_data.py
@@ -1,22 +1,33 @@
+"""
+Extracts data from savefiles and saves into seperate csv files
+"""
+
 import configparser
 import csv
 
-with open('data/saveData1.ini', 'r') as file:
-    content = file.read().splitlines()[:-1]
-
-config_reader = configparser.ConfigParser()
-config_reader.read_string('\n'.join(content))
-
-game_data = config_reader['Game']
+file_names = ['data/saveData1.ini', 'data/saveData2.ini', 'data/saveData3.ini']
 
 keys = ['percent', 'minutes', 'seconds', 'snotty']
 
-values = [int(float(game_data.get(key, '0.000000').strip('"'))) for key in keys]
+for index, file_name in enumerate(file_names, start=1):
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            content = file.read().splitlines()[:-1]
 
-headers = keys
-rows = [values]
+        config_reader = configparser.ConfigParser()
+        config_reader.read_string('\n'.join(content))
 
-with open('data/save_file_1.csv', 'w', newline="") as output:
-    writer = csv.writer(output)
-    writer.writerow(headers)
-    writer.writerows(rows)
+        game_data = config_reader['Game']
+
+        rows = [int(float(game_data.get(key, '0.000000').strip('"'))) for key in keys]
+
+        headers = keys
+
+        output_file = f'data/save_file_{index}.csv'
+
+        with open(output_file, 'w', newline="", encoding='utf-8') as output:
+            writer = csv.writer(output)
+            writer.writerow(headers)
+            writer.writerow(rows)
+    except FileNotFoundError:
+        print(f"File {file_name} not found. Skipping...")
